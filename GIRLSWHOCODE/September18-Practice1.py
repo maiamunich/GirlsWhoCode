48ddeea71faa85a1f94bb3241f68751737ddac00
Maia <maia.munich@gmail.com>
2025-09-28 23:21:35 -0400

    Commiting Changes

---
+++ GIRLSWHOCODE/September18-Practice1.py
@@ -0,0 +1,53 @@
+#Maia Munich 
+#Project practice #1 
+#September 18, 2025
+
+# hashtags are used to make comments in Python as well as 
+# three quotation marks at the top and bottom of the section you want covered
+
+
+#Printing in Python 
+print("Hi, My name is Maia Munich")
+print(3+2)
+print("3+2")
+
+
+#Variables in Python 
+name =  "Maia"
+age = 21
+print("My name is", name, "and I am", age,"yrs old")
+
+#Math in Python 
+a = 5
+b = 3
+
+print(a+b)  #+ is used to add
+print(a*b)  #* is used to multiply
+print(a/b)  #/ is used to divide and return a float
+print(a//b) #// is used to divide and round down to the nearest integer
+print(a%b) #% is used to get the remainder of the division
+
+#User Input in Python 
+c = int(input("Enter a number: "))
+d = int(input("Enter another number: "))
+print(c+d)
+
+
+
+#If/Else Decision 
+
+temperature = 85 #this is a variable that stores the temperature
+
+ #if the temperature is greater than 100, print "Damn Its Hot"
+if temperature > 100:
+    print( "Damn Its Hot")
+else:   #else is used to print "Its a nice day" if the temperature is less than 100
+    print("Its a nice day") 
+
+
+temp = int(input("How many degrees is it?: ")) #this is a variable that stores the temperature
+
+if temp > 100: #if the temperature is greater than 100, print "Damn Its Hot"
+    print( "Damn Its Hot")
+else:
+    print("Its a nice day")
